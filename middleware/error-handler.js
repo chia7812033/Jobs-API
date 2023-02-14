99d3a0c786c54d2f32094b71c3af98d1d3e82663
chia7812033 <dan891221@gmail.com>
2023-02-14 20:51:43 +0800

    Set up custom server error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,34 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+  let customErr = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, please try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // Email is not unique
+  if (err.code && err.code === 11000) {
+    customErr.msg = `${Object.keys(err.keyValue)} is duplicated`
+    customErr.statusCode = StatusCodes.BAD_REQUEST
+  }
+
+  // Validation fails
+  if (err.name === 'ValidationError') {
+    customErr.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ')
+    customErr.statusCode = StatusCodes.BAD_REQUEST
+  }
+
+  // Job id format is wrong
+  if (err.name === 'CastError') {
+    customErr.msg = `Cannot found the job with id ${err.value}`
+    customErr.statusCode = StatusCodes.NOT_FOUND
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customErr.statusCode).json({ msg: customErr.msg })
 }
 
 module.exports = errorHandlerMiddleware
